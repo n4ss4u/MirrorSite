c8de2c06fb058e3908f393e883cbcb11f1aa01b7
n4ss4u <148468420+n4ss4u@users.noreply.github.com>
2023-11-11 17:34:30 -0500

    Add files via upload

---
+++ mirrorsite.py
@@ -0,0 +1,38 @@
+import requests
+from colorama import Fore as f, Back as b, Style
+import random 
+
+# BANNER 
+print(f.YELLOW + "┌──────────────────o───────────────────┐\n│" + f.RED + "           Creador: " + f.BLUE + "n4ss4u            " + f.YELLOW + "│\n│" + f.RED + "          Script: " + f.BLUE + "MirrorSite          " + f.YELLOW + "│\n│" + f.RED +  "         Version: " + f.BLUE + " v0.1 - 2023         "  + f.YELLOW + "│")
+print(f.YELLOW + "├──────────────────o───────────────────┤\n│" + f.RED + "     Introduzca la URL del sitio      " + f.YELLOW + "│\n│" + f.RED + "   Ejemplo: https://www.ejemplo.com   " + f.YELLOW + "│\n├──────────────────────────────────────┘")
+
+website = input(f.YELLOW + "│\n└─[ + " + f.RED + "MirrorSite" + f.YELLOW +"]>> " + Style.RESET_ALL)
+
+try:
+	response = requests.get(website)
+
+except:
+	print("\n[!] Error, el sitio no se pudo clonar porque no existe.")
+
+
+
+if "response" in locals() and response.status_code == 200:
+	name_save_file = website 
+
+	if "https://" in website:
+		name_save_file = name_save_file.replace("https://", "")
+
+	elif "http://" in website:
+		name_save_file = name_save_file.replace("http://", "")
+
+	# GENERARAR NOMBRE PARA GUARDAR EL ARCHIVO
+	name_save_file = name_save_file.replace("/", "")
+	name_save_file = name_save_file + str(random.randint(1000, 9999))
+
+	# GURADANDO CONTENIDO EN EL ARCHIVO
+	file = open(name_save_file + ".html", "w", encoding="utf-8")
+	file.write(str(response.text))
+	file.close()
+
+	print("\n[!] El sitio clonado se guardó como: " + name_save_file + ".index")
+
